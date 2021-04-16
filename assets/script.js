6590b2466d310007bd57a724f773617f8607dd10
Anthony Ndegwa <anthon.n24@gmail.com>
2021-04-16 15:40:34 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,60 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
+$(document).ready(function) () { 
+
+var cityStorage = localStorage; 
+var cityList = []; 
+const APIKEY = "747881da8c4d68c9b809583b4d0c58bb"; 
+var targetCity = ""; 
+var targetCityLon = ""; 
+var targetCityLat = ""; 
 
 
-button.addEventListener('click', function(){   
-fetch('api.openweathermap.org/data/2.5/weather?q={city name}'+inputValue.value+'&appid={747881da8c4d68c9b809583b4d0c58bb}') 
-  .then(response => response.json()) 
-  .then(data => { 
-    var nameValue = data ['name']
+function retrievePastSearches() { 
+  if(cityStorage.getItem("pastWeatherCities") != undefined) { 
+    cityList = JSON.parse(cityStorage.getItem("pastWeatherCities")); 
+    for (var i = 0; i < cityList.length; i++) { 
 
+      var NewRecentSearchLink - $("<a href=\"\#\"></a>"); 
+      NewRecentSearchLink.text(cityList[i]); 
+      NewRecentSearchLink.attr("data-city", cityList[i]); 
+      NewRecentSearchLink.attr("class","recentSearchItem list-group-item-action"); 
+      $("#resultList").prepend(newRecentSearchLink); 
+    }
   }
+}
 
-   
+ retrievePastSearches(); 
 
-  .catch(errr => alert('Wrong city name!!'))
+ function savePastSearches() { 
+   cityStorage.setItem("pastWeatherCities", JSON.stringify(cityList)); 
+
+ }
+
+ $("#searchButton").on("click", function () { 
+
+  event.preventDefault; 
+  console.log(`Searching for ${$("#searchTermEntry").val()} data.`);
+        targetCity = $("#searchTermEntry").val();
+
+      getCurrentWeatherData(targetCity); 
+ }); 
+
+
+ function getCurrentWeatherData(cityName) {  
+    $.ajax({ 
+      method: "GET", 
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + `${cityName}&appid=${APIKEY}`
+    }).then(function (currentResponse) {
+
+      
 
 
 
-})
+    })
+
+ }
+}
+
+
+
+
+
